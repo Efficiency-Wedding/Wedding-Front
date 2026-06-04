49361f9c1129fa9a99b553eb0399fbe579c7fe26
Antonio Andriam <andriamamolisoa@gmail.com>
2026-06-04 10:20:49 +0200

    add: ajout de nouvelles photos

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -44,7 +44,7 @@ export function BlogPage() {
           </div>
         )}
       </main>
-      <BlogFooter />
+      {!selected && <BlogFooter />}
     </div>
   );
 }
--- src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import Home from "@/components/sections/home";
-
-export const Route = createFileRoute("/")({
-  component: Home,
-});
