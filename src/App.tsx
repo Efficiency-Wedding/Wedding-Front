592f5818f36f601972311d23bd5c7d68dc26c8e6
nandrianinaharimalala <nandrianinaharimalala@gmail.com>
2026-06-04 13:23:07 +0300

    About ajouté

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Apropos from "./about/Apropos";
+import HistoirePage from "./about/HistoirePage";
+import Travail from "./about/Travail";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <p className='text-red-500'>Projet EFFICIENCY WEDDING</p>
-        <p> React Vite</p>
-      </div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Apropos />} />
+        <Route path="/Histoirepage" element={<HistoirePage />} />
+        <Route path="/Travail" element={<Travail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
